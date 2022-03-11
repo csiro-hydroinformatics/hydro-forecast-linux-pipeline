c5e078c13eff9718ce0857e0ed115623de7076b7
J-M <jean-michel.perraud@csiro.au>
2022-03-11 16:44:01 +1100

    WIP pipeline to build debian packges of swift and co

--- packages/build-packages.sh
+++ packages/build-packages.sh
@@ -11,18 +11,18 @@ echo from within build-packages.sh: TEST_PAT=${TEST_PAT} LOCAL_TEST_PAT_ENV_VAR=
 export IMAGE_NAME=swift_builder
 export CURRENT_UID=$(id -u):$(id -g)
 
-export ARTIFACT_PATH=$(pwd)/pkgs
-mkdir -p ${ARTIFACT_PATH}
+# export ARTIFACT_PATH=$(pwd)/pkgs
+# mkdir -p ${ARTIFACT_PATH}
 
 echo CURRENT_UID=${CURRENT_UID}
 # echo SRC_PATH=${SRC_PATH}
 
 # docker build --no-cache --force-rm --build-arg CURRENT_UID_ARG=${CURRENT_UID} -t ${IMAGE_NAME} .
 docker build --build-arg CURRENT_UID_ARG=${CURRENT_UID} -t ${IMAGE_NAME} .
-docker run --rm --name ${IMAGE_NAME} -v "${ARTIFACT_PATH}:${OUT_ARTIFACT_PATH}" ${IMAGE_NAME} ${SWIFT_PAT} ${TEST_PAT} 
+docker run --rm --name ${IMAGE_NAME} -v "${OUT_ARTIFACT_PATH}:/pkgs/" ${IMAGE_NAME} ${SWIFT_PAT} ${TEST_PAT} 
 
-echo ARTIFACT_PATH=${ARTIFACT_PATH} contains:
-ls ${ARTIFACT_PATH}
+echo build-packages.sh: OUT_ARTIFACT_PATH = ${OUT_ARTIFACT_PATH} contains:
+ls ${OUT_ARTIFACT_PATH}
 
 ## Obsolete?
 # docker rm -f ${IMAGE_NAME}
