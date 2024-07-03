a9a82418ff9b811ef8b97d8b72716561c47622c6
J-M <jean-michel.perraud@csiro.au>
2024-07-03 13:54:48 +1000

    Suspend the use of git LFS for sample data retrieval; use FTP instead

--- packages/unzip_testdata.sh
+++ packages/unzip_testdata.sh
@@ -17,25 +17,40 @@ cd ${ROOT_DATA_DIR}
 ret_code=$?
 _exit_if_failed $ret_code "Failed to change dir to ${ROOT_DATA_DIR}"
 
-# sf-test-data.git
-ls sf-test-data/
-ret_code=$?
-_exit_if_failed $ret_code "Failed: sf-test-data not found under ${ROOT_DATA_DIR}"
+# Jul 24: deprecate as makes it more difficult than useful for others to adapt the pipeline
+# # sf-test-data.git
+# ls sf-test-data/
+# ret_code=$?
+# _exit_if_failed $ret_code "Failed: sf-test-data not found under ${ROOT_DATA_DIR}"
 mkdir -p ${ROOT_DATA_DIR}/swift
 mkdir -p ${ROOT_DATA_DIR}/chypp
-mv sf-test-data/swift_test_data.7z swift/
-mv sf-test-data/chypp_test_data.7z chypp/
+# mv sf-test-data/swift_test_data.7z swift/
+# mv sf-test-data/chypp_test_data.7z chypp/
+
+curl -o ${ROOT_DATA_DIR}/chypp/chypp_test_data.7z ftp://ftp.csiro.au/hfc/chypp_test_data.7z
+ret_code=$?
+_exit_if_failed $ret_code "chypp_test_data.7z download failed"
+
+curl -o ${ROOT_DATA_DIR}/swift/swift_test_data.7z ftp://ftp.csiro.au/hfc/swift_test_data.7z
+ret_code=$?
+_exit_if_failed $ret_code "swift_test_data.7z download failed"
+
 # First, let us check we have the sample data file installed. Otherwise no point.
 # The data is present on the base image we use for the pipeline, not checked out live by the pipeline to save runtime.
 if [ ! -e ${CSIRO_BITBUCKET}/swift/bindings/R/pkgs/swift/data/ ]; then
     echo ERROR: ${CSIRO_BITBUCKET}/swift/bindings/R/pkgs/swift/data/ not found
     exit ${_ENOENT}; 
 fi
-if [ ! -e ${ROOT_DATA_DIR}/sf-test-data/swift_sample_data.rda ]; then
-    echo ERROR: 'swift_sample_data.rda' not found, expected ${ROOT_DATA_DIR}/sf-test-data/swift_sample_data.rda 
-    exit ${_ENOENT}; 
-fi
-mv sf-test-data/swift_sample_data.rda ${CSIRO_BITBUCKET}/swift/bindings/R/pkgs/swift/data/
+
+# if [ ! -e ${ROOT_DATA_DIR}/sf-test-data/swift_sample_data.rda ]; then
+#     echo ERROR: 'swift_sample_data.rda' not found, expected ${ROOT_DATA_DIR}/sf-test-data/swift_sample_data.rda 
+#     exit ${_ENOENT}; 
+# fi
+curl -o ${ROOT_DATA_DIR}/swift/swift_sample_data.rda ftp://ftp.csiro.au/hfc/swift_sample_data.rda
+ret_code=$?
+_exit_if_failed $ret_code "swift_sample_data.rda download failed"
+
+mv ${ROOT_DATA_DIR}/swift/swift_sample_data.rda ${CSIRO_BITBUCKET}/swift/bindings/R/pkgs/swift/data/
 
 cd ${ROOT_DATA_DIR}/swift/
 7z x swift_test_data.7z 
