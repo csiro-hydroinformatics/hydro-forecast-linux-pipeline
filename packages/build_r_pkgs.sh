267935deb5848a27fc060ef12684691b3e5ec3f0
J-M <jean-michel.perraud@csiro.au>
2026-04-27 14:43:50 +1000

    ci: skip building R vignettes holding up pipeline

--- packages/build_r_pkgs.sh
+++ packages/build_r_pkgs.sh
@@ -60,9 +60,9 @@ R_REPO_DIR=${ROOT_OUT_DIR}/lib/R_repo
 R_SRC_REPO_PATH=${R_REPO_DIR}/src/contrib
 mkdir -p $R_SRC_REPO_PATH
 
-# March 2025 try to get past an issue on the pipeline agent (ubuntu latest) not reproducible on Linux Debian.
-# RCMD_BUILD_OPT= --no-build-vignettes
-RCMD_BUILD_OPT=
+# https://github.com/csiro-internal/swift/issues/7 have to disable.
+RCMD_BUILD_OPT= --no-build-vignettes
+# RCMD_BUILD_OPT=
 
 R_EXE=R
 R_VANILLA="${R_EXE} --no-save --no-restore-data"
